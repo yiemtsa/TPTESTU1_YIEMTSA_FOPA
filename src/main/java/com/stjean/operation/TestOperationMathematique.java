bbdc041803264f8471eb7a6b3f2870395cef7d2f
steeven forester <steevenforester@gmail.com>
2024-09-30 05:06:41 +0200

    creation de la class test

---
+++ src/main/java/com/stjean/operation/TestOperationMathematique.java
@@ -0,0 +1,23 @@
+package com.stjean.operation;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestOperationMathematique {
+	
+	 @Test
+	    public void testEstPositif() {
+	        assertEquals(true, OperationMathematique.estPositif(3));
+	        assertEquals(false, OperationMathematique.estPositif(-5));
+	        assertEquals(true, OperationMathematique.estPositif(2));
+	    }
+
+	    @Test
+	    public void testFactoriel() {
+	        assertEquals(24, OperationMathematique.factoriel(4));
+	        assertEquals(2, OperationMathematique.factoriel(2));
+	        assertEquals(6, OperationMathematique.factoriel(3));
+	    }
+
+}
