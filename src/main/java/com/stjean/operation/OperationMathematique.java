8188f7628b53927454ac21123d6474500777385e
steeven forester <steevenforester@gmail.com>
2024-09-30 04:53:46 +0200

    creation de la methode factoriel

--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -4,5 +4,16 @@ public class OperationMathematique {
 	public static boolean estPositif(int nombre) {
         return nombre > 0;
     }
+	
+	 public static int factoriel(int nombre) {
+	        if (nombre < 0) {
+	            throw new IllegalArgumentException("Le nombre doit être positif ou zéro.");
+	        }
+	        int resultat = 1;
+	        for (int i = 1; i <= nombre; i++) {
+	            resultat *= i;
+	        }
+	        return resultat;
+	    }
 
 }
