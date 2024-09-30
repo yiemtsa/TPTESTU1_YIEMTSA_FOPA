f687c037428f72adf6d484d16b225288a567890e
steeven forester <steevenforester@gmail.com>
2024-09-30 05:49:28 +0200

    creation de la classe pour lever une exception lor d'un factoriel negatif

---
+++ src/main/java/com/stjean/operation/IllegalParamISIException.java
@@ -0,0 +1,14 @@
+package com.stjean.operation;
+
+public class IllegalParamISIException  extends RuntimeException {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public IllegalParamISIException(String message) {
+        super(message);
+    }
+
+}
--- src/main/java/com/stjean/operation/OperationMathematique.java
+++ src/main/java/com/stjean/operation/OperationMathematique.java
@@ -20,13 +20,22 @@ public class OperationMathematique {
 	    }
 	 
 	 public static Integer[] trier(int[] listes) {
-	        // Convertir le tableau d'int en tableau d'Integer
+	       
 	        Integer[] listeInteger = Arrays.stream(listes).boxed().toArray(Integer[]::new);
 	        
-	        // Trier le tableau par ordre décroissant
 	        Arrays.sort(listeInteger, Collections.reverseOrder());
 	        
 	        return listeInteger;
+	    }
+	 public static int factorielNegatif(int nombre) {
+	        if (nombre < 0) {
+	            throw new IllegalParamISIException("Le factoriel n'est pas défini pour les nombres négatifs.");
+	        }
+	        int resultat = 1;
+	        for (int i = 1; i <= nombre; i++) {
+	            resultat *= i;
+	        }
+	        return resultat;
 	    }
 
 }
